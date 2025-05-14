20c7f1b11982a149d4ee472773f640082466b4b7
Alaedddine718 <adaoudi@student.universidadunie.com>
2025-05-14 10:00:40 +0200

    tres

---
+++ src/scheduler.py
@@ -0,0 +1,26 @@
+from proceso import Proceso
+
+class Scheduler:
+    def planificar(self, procesos):
+        raise NotImplementedError("Este método debe ser implementado por las subclases")
+
+
+class FCFSScheduler(Scheduler):
+    def planificar(self, procesos):
+        tiempo = 0
+        gantt = []
+        for p in procesos:
+            p.tiempo_inicio = tiempo
+            tiempo += p.duracion
+            p.tiempo_fin = tiempo
+            gantt.append((p.pid, p.tiempo_inicio, p.tiempo_fin))
+        return gantt
+
+
+class RoundRobinScheduler(Scheduler):
+    def __init__(self, quantum):
+        if quantum <= 0:
+            raise ValueError("El quantum debe ser un entero positivo.")
+        self.quantum = quantum
+
+    
