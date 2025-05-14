d85b9f9a558554b5288aa2fba5c27bd8ca55da9b
Alaedddine718 <adaoudi@student.universidadunie.com>
2025-05-14 09:48:14 +0200

    dos

--- src/proceso.py
+++ src/proceso.py
@@ -4,4 +4,10 @@ class Proceso:
             raise ValueError("El PID no puede estar vacío.")
         if duracion <= 0:
             raise ValueError("La duración debe ser positiva.")
-       
+        self.pid = pid
+        self.duracion = duracion
+        self.prioridad = prioridad
+        self.tiempo_restante = duracion
+        self.tiempo_llegada = 0
+        self.tiempo_inicio = None
+        self.tiempo_fin = None
