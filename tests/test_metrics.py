bd92c294c83005c7c68503bde09d41cc6903bbd6
Alaedddine718 <adaoudi@student.universidadunie.com>
2025-05-14 10:19:02 +0200

    test met uno

---
+++ tests/test_metrics.py
@@ -0,0 +1,19 @@
+import unittest
+from proceso import Proceso
+from metrics import calcular_metricas
+
+class TestMetrics(unittest.TestCase):
+    def test_metricas(self):
+        p1 = Proceso("P1", 4, 1)
+        p2 = Proceso("P2", 2, 1)
+        p1.tiempo_inicio = 0
+        p1.tiempo_fin = 4
+        p2.tiempo_inicio = 4
+        p2.tiempo_fin = 6
+
+        metricas = calcular_metricas([p1, p2])
+        self.assertAlmostEqual(metricas["respuesta_media"], 2.0)
+        self.assertAlmostEqual(metricas["retorno_media"], 3.0)
+        self.assertAlmostEqual(metricas["espera_media"], 1.0)
+
+
