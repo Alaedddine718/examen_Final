41095cb00b76340a19d55d65ce3f71215e9f804c
Alaedddine718 <adaoudi@student.universidadunie.com>
2025-05-14 10:18:16 +0200

    test repo dos

--- tests/test_repositorio.py
+++ tests/test_repositorio.py
@@ -10,4 +10,23 @@ class TestRepositorio(unittest.TestCase):
         repo.agregar(p)
         self.assertEqual(len(repo.listar()), 1)
 
-  
+    def test_pid_duplicado(self):
+        repo = RepositorioProcesos()
+        p1 = Proceso("X", 4, 1)
+        p2 = Proceso("X", 3, 2)
+        repo.agregar(p1)
+        with self.assertRaises(ValueError):
+            repo.agregar(p2)
+
+    def test_guardar_y_cargar_json(self):
+        repo = RepositorioProcesos()
+        repo.agregar(Proceso("A", 5, 1))
+        repo.guardar_json("test.json")
+
+        nuevo_repo = RepositorioProcesos()
+        nuevo_repo.cargar_json("test.json")
+        self.assertEqual(len(nuevo_repo.listar()), 1)
+        os.remove("test.json")
+
+if __name__ == "__main__":
+    unittest.main() 
