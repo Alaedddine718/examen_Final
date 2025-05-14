48ef09f0aefe97e0a3d2677778d6e7677512e562
Alaedddine718 <adaoudi@student.universidadunie.com>
2025-05-14 09:56:18 +0200

    uno

--- src/proceso.py
+++ src/proceso.py
@@ -1,13 +1,9 @@
 class Proceso:
-    def __init__(self, pid: str, duracion: int, prioridad: int):
+    def __init__(self, pid, duracion, prioridad):
         if not pid:
             raise ValueError("El PID no puede estar vacío.")
         if duracion <= 0:
             raise ValueError("La duración debe ser positiva.")
-        self.pid = pid
-        self.duracion = duracion
-        self.prioridad = prioridad
-        self.tiempo_restante = duracion
-        self.tiempo_llegada = 0
-        self.tiempo_inicio = None
-        self.tiempo_fin = None
+      
+
+
